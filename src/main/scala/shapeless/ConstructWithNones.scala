adb84772c3ed29be341ea468672a41c9e9739505
fokot <frantisek.kocun@gmail.com>
2017-10-13 12:07:54 +0200

    amazing shapeless

---
+++ src/main/scala/shapeless/ConstructWithNones.scala
@@ -0,0 +1,28 @@
+package shapeless
+
+
+object ConstructWithNones extends App {
+
+  implicit val emptyHlist = HNil
+
+  implicit def addNoneToHList[A, H <: HList](implicit h: H): Option[A] :: H = None :: h
+
+  // I needed to use `kinda curried type parameter`
+  // https://tpolecat.github.io/2015/07/30/infer.html
+  final case class WithAllNones[A]() {
+    def apply[H <: HList]()(implicit gen: Generic.Aux[A, H],  h: H): A = gen.from(h)
+  }
+
+  // if class has all fields Options it will create instance of class with all `None`
+  def createWithAllNones[A] = new WithAllNones[A]
+
+  case class A()
+  case class B(notAOption: Int)
+  case class C(b: Option[Int])
+  case class D(b: Option[Int], a: Option[A])
+
+  println(createWithAllNones[A]())
+//  println(createWithAllNones[B])
+  println(createWithAllNones[C]())
+  println(createWithAllNones[D]())
+}
--- src/main/scala/shapeless/TwoClassesToList.scala
+++ src/main/scala/shapeless/TwoClassesToList.scala
@@ -1,62 +1,63 @@
-package shapeless
-
-import shapeless._
-import shapeless.labelled.{FieldType, field}
-import shapeless.ops.hlist.{Align, ToTraversable, ZipWith}
-
-object TwoClassesToList {
-
-  // HLIST
-  def hlistZipArgsToList[A, B, P <: Poly2, ARepr <: HList, BRepr <: HList, R <: HList]
-  (a: A, b: B, f: P)(
-     implicit
-     aGen : Generic.Aux[A, ARepr],
-     bGen : Generic.Aux[B, BRepr],
-     zipWith : ZipWith.Aux[ARepr, BRepr, P, R],
-     toTrav: ToTraversable.Aux[R, List, String]
-   ): List[String] =
-      aGen.to(a).zipWith(bGen.to(b))(f).toList
-
-
-  object hlistSum extends Poly2 {
-    implicit def repr[A]: Case.Aux[A, A, String] = at[A, A]( (a, b) => (a, b).toString)
-  }
-
-  // RECORD
-  def recordZipArgsToList[A, B, P <: Poly2, ARepr <: HList, BRepr <: HList, R <: HList]
-  (a: A, b: B, f: P)(
-     implicit
-     aGen : LabelledGeneric.Aux[A, ARepr],
-     bGen : LabelledGeneric.Aux[B, BRepr],
-     align : Align[ARepr, BRepr],
-     zipWith : ZipWith.Aux[BRepr, BRepr, P, R],
-     toTrav: ToTraversable.Aux[R, List, String]
-   ): List[String] =
-      align.apply(aGen.to(a)).zipWith(bGen.to(b))(f).toList
-
-
-  object recordSum extends Poly2 {
-    implicit def repr[K <: Symbol, V] = at[FieldType[K, V], FieldType[K, V]] { (a, b) =>
-      field[K]( (a, b).toString )
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    case class Data(id: Long, s2: String, s: String)
-    case class DataFilter(id: Long, s: String)
-    case class DataFilter2(id: Long, s: String, s2: String)
-
-    val d = Data(5, "first", "nothing")
-    val f = DataFilter(30, "second")
-    val f2 = DataFilter2(30, "xxx", "third")
-
-    val v = hlistZipArgsToList(d, f, hlistSum)
-    println(v)
-
-//    val w = recordZipArgsToList(d, f, recordSum)
-//    println(w)
-
-    val x = recordZipArgsToList(d, f2, recordSum)
-    println(x)
-  }
-}
+//package shapeless
+//
+//import shapeless._
+//import shapeless.labelled.{FieldType, field}
+//import shapeless.ops.hlist.{Align, ToTraversable, ZipApply, ZipWith}
+//
+//object TwoClassesToList {
+//
+//  // HLIST
+//  def hlistZipArgsToList[A, B, P <: Poly2, ARepr <: HList, BRepr <: HList, R <: HList]
+//  (a: A, b: B, f: P)(
+//     implicit
+//     aGen : Generic.Aux[A, ARepr],
+//     bGen : Generic.Aux[B, BRepr],
+//     zipWith : ZipWith.Aux[ARepr, BRepr, P, R],
+//     toTrav: ToTraversable.Aux[R, List, String]
+//   ): List[String] =
+//      aGen.to(a).zipWith(bGen.to(b))(f).toList
+//
+//
+//  object hlistSum extends Poly2 {
+//    implicit def repr[A]: Case.Aux[A, A, String] = at[A, A]( (a, b) => (a, b).toString)
+//  }
+//
+//  // RECORD
+//  def recordZipArgsToList[A, B, P <: Poly2, ARepr <: HList, BRepr <: HList, R <: HList]
+//  (a: A, b: B, f: P)(
+//     implicit
+//     aGen : LabelledGeneric.Aux[A, ARepr],
+//     bGen : LabelledGeneric.Aux[B, BRepr],
+//     align : Align[ARepr, BRepr],
+////     zipWith : ZipWith.Aux[BRepr, BRepr, P, R],
+//     za : ZipApply.Aux[BRepr, BRepr, P, R],
+//     toTrav: ToTraversable.Aux[R, List, String]
+//   ): List[String] =
+//      align.apply(aGen.to(a)).zipWith(bGen.to(b))(f).toList
+//
+//
+//  object recordSum extends Poly2 {
+//    implicit def repr[K <: Symbol, V] = at[FieldType[K, V], FieldType[K, V]] { (a, b) =>
+//      field[K]( (a, b).toString )
+//    }
+//  }
+//
+//  def main(args: Array[String]): Unit = {
+//    case class Data(id: Long, s2: String, s: String)
+//    case class DataFilter(id: Long, s: String)
+//    case class DataFilter2(id: Long, s: String, s2: String)
+//
+//    val d = Data(5, "first", "nothing")
+//    val f = DataFilter(30, "second")
+//    val f2 = DataFilter2(30, "xxx", "third")
+//
+//    val v = hlistZipArgsToList(d, f, hlistSum)
+//    println(v)
+//
+////    val w = recordZipArgsToList(d, f, recordSum)
+////    println(w)
+//
+//    val x = recordZipArgsToList(d, f2, recordSum)
+//    println(x)
+//  }
+//}
